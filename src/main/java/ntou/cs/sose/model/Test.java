e0a2dd12303aed870a1cf8c3d12cfd4d441ccd14
gn01868184 <gn01868184@gmail.com>
2021-02-17 13:14:03 +0800

    refactor: test

--- src/main/java/ntou/cs/sose/model/Test.java
+++ src/main/java/ntou/cs/sose/model/Test.java
@@ -3,6 +3,14 @@ package ntou.cs.sose.model;
 public class Test {
 	public static void main(String[] args) {
 		SwaggerChecker sc = new SwaggerChecker();
-		sc.test();
+		System.out.println("jsonSchema:");
+		sc.jsonSchema();
+		System.out.println("jsonPath:");
+		sc.jsonPath();
+		System.out.println("jsonParse:");
+		sc.jsonParse();
+		System.out.println("chatbotFlow:");
+		sc.chatbotFlow();
+		
 	}
 }
