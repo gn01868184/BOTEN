c54f3362a24e859f85a9a520b7b41b4dc0cf8caa
gn01868184 <gn01868184@gmail.com>
2021-02-17 17:02:18 +0800

    refactor: checkChatbotFlow

--- src/main/java/ntou/cs/sose/model/SwaggerChecker.java
+++ src/main/java/ntou/cs/sose/model/SwaggerChecker.java
@@ -1,6 +1,7 @@
 package ntou.cs.sose.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 
@@ -51,44 +52,59 @@ public class SwaggerChecker {
 	    
 	    return null;
 	}
-	public String jsonParse() {
-	    if (swagger.has("paths")) {
-	    	  Iterator<String> keys = swagger.keys();
-	    	  ArrayList arrL = new ArrayList();
-	    	  while (keys.hasNext()) {
-	    	    String key = keys.next();
-	    	    arrL.add(key);
-	    	  }
-	    	  for(int i=0;i<arrL.size();i++) {
-	    	    System.out.println(arrL.get(i));
-	    	  }
-	    }
-	    return null;
-	}
-	public String chatbotFlow() {
+	public String checkChatbotFlow() {
 		// 檢查info-x-chatbotFlow與Swagger內的path是否對應
 	    if (swagger.has("info")) {
 	    	JSONObject info = swagger.getJSONObject("info");
 		    if (info.has("x-chatbotFlow")) {
-		    	org.json.JSONArray chatbotFlow = info.getJSONArray("x-chatbotFlow");
-		    	System.out.println(chatbotFlow);
-		    	for (int i = 0; i < chatbotFlow.length(); i++) {            
-		    	    org.json.JSONArray flowArray = chatbotFlow.getJSONObject(i).getJSONArray("flow");
-		    	    String flowName = chatbotFlow.getJSONObject(i).getString("flowName");
-		    	    System.out.println(flowName);
-			    	for (int j = 0; j < flowArray.length(); j++) {            
-			    	    String flow = flowArray.getString(j);
-			    	    System.out.println(flow);
+		    	HashMap<String, Object> allFlow = getChatbotFlow();
+		    	for (Object key : allFlow.keySet()) {
+		    	     System.out.println(key + " : " + allFlow.get(key));
+		    	     ArrayList flowArr = (ArrayList) allFlow.get(key);
+		    	     for (int i = 0; i < flowArr.size(); i++) { 
 			    	    try {
-			    	    	LinkedHashMap<Object, Object> path = JsonPath.read(swagger.toString(), "$.paths." + flow);
-			    	    	System.out.println("success");
+			    	    	LinkedHashMap<Object, Object> path = JsonPath.read(swagger.toString(), "$.paths." + flowArr.get(i));
+			    	    	System.out.println("$.paths." + flowArr.get(i) + ": success");
 			    	    }catch (PathNotFoundException e) {
 			    	    	System.out.println(e.getMessage());
+			    	    	System.out.println("#/info/x-chatbotFlow/" + i + "/flow: [" + flowArr.get(i) + "] endpoint not found in Swagger");
 			    	    }
-			    	}
-		    	}
-		    }
-	    }
+		    	     }
+				}
+			}
+		}
+
 	    return null;
+	}
+	public HashMap<String, Object> getChatbotFlow() {
+    	JSONObject info = swagger.getJSONObject("info");
+    	org.json.JSONArray chatbotFlow = info.getJSONArray("x-chatbotFlow");
+    	HashMap<String, Object> allFlow = new HashMap<String, Object>();
+    	ArrayList pathsArr = new ArrayList();
+    	for (int i = 0; i < chatbotFlow.length(); i++) { 
+    		pathsArr.clear();
+    	    org.json.JSONArray flowArray = chatbotFlow.getJSONObject(i).getJSONArray("flow");
+    	    String flowName = chatbotFlow.getJSONObject(i).getString("flowName");
+	    	for (int j = 0; j < flowArray.length(); j++) {            
+	    	    String flow = flowArray.getString(j);
+	    	    pathsArr.add(flow);
+	    	}
+	    	allFlow.put(flowName, pathsArr);
+    	}
+    	System.out.println("allFlow:");
+    	System.out.println(allFlow);
+	    return allFlow;
+	}
+	public ArrayList getAllPath() {
+    	org.json.JSONObject paths = swagger.getJSONObject("paths");
+    	Iterator<String> keys = paths.keys();
+    	  ArrayList pathsArr = new ArrayList();
+    	  while (keys.hasNext()) {
+    	    String key = keys.next();
+    	    pathsArr.add(key);
+    	  }
+    	  System.out.println("all path:");
+    	  System.out.println(pathsArr);
+    	  return pathsArr;
 	}
 }
