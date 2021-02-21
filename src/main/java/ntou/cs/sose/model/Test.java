a4a7fddb260aacf02bd9f794bead980c26ee2e5d
gn01868184 <gn01868184@gmail.com>
2021-02-21 11:58:36 +0800

    refactor: test

--- src/main/java/ntou/cs/sose/model/Test.java
+++ src/main/java/ntou/cs/sose/model/Test.java
@@ -1,8 +1,14 @@
 package ntou.cs.sose.model;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class Test {
 	public static void main(String[] args) {
+		Test te = new Test();
+	    Class tes = te.getClass();
+		JSONObject swagger = new JSONObject(new JSONTokener(tes.getResourceAsStream("/foursquareSwagger.json")));
 		SwaggerChecker sc = new SwaggerChecker();
-		sc.sc();
+		sc.sc(swagger);
 	}
 }
