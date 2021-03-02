01a998b871af370ea6ff3752941113d036356b22
gn01868184 <gn01868184@gmail.com>
2021-03-02 14:16:16 +0800

    refactor: HttpConnection

--- src/main/java/ntou/cs/sose/controller/ChatbotController.java
+++ src/main/java/ntou/cs/sose/controller/ChatbotController.java
@@ -8,13 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
+import ntou.cs.sose.model.HttpConnection;
+import ntou.cs.sose.model.InputOutputHandler;
 import ntou.cs.sose.model.SwaggerChecker;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.*;
-import java.util.stream.Collectors;
 
 import org.json.*;
 
@@ -22,47 +18,33 @@ import org.json.*;
 @RequestMapping("/chatbot")
 public class ChatbotController {
 	String swagger;
-	
-	 @RequestMapping("/home")
-	    public String greeting() {
-	        return "index";
-	    }
-	
+	JSONObject swaggerObj = new JSONObject();
+	Gson gson = new Gson();
+
+	@RequestMapping("/home")
+	public String greeting() {
+		return "index";
+	}
+
 	@GetMapping(value = "/swaggerCheck", produces = "application/json")
 	@ResponseBody
-    public String swaggerCheck(@RequestParam String swaggerURL) {
-		try {
-			URL url = new URL(swaggerURL);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "application/json; utf-8");
-			
-			BufferedReader br = null;
-			if (200 <= con.getResponseCode() && con.getResponseCode() <= 399) {
-			    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			    String responseBody = br.lines().collect(Collectors.joining());
-			    swagger = responseBody;
-			    JSONObject jsonObject = new JSONObject(responseBody);
-			    System.out.println(jsonObject);
-				SwaggerChecker sc = new SwaggerChecker();
-				Gson gson = new Gson();
-				String json = gson.toJson(sc.sc(jsonObject)); 
-			    return json.toString();
-			} else {
-			    System.out.println("Error: " + con.getResponseCode());
-			}
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		  }
-		return null;
-    }
-	
-	@GetMapping(value = "/s", produces = "application/json")
+	public String swaggerCheck(@RequestParam String swaggerURL) {
+		HttpConnection httpConnection = new HttpConnection();
+		swagger = httpConnection.connection(swaggerURL);
+		swaggerObj = new JSONObject(swagger);
+		System.out.println(swaggerObj);
+
+		SwaggerChecker sc = new SwaggerChecker();
+		String json = gson.toJson(sc.sc(swaggerObj));
+		return json.toString();
+	}
+
+	@GetMapping(value = "/InputOutputHandler", produces = "application/json")
 	@ResponseBody
-    public String test() {
-		return swagger;
-    }
+	public String test() {
+		InputOutputHandler ioc = new InputOutputHandler();
+		String json = gson.toJson(ioc.ioc(swaggerObj));
+		return json.toString();
+	}
 
 }
