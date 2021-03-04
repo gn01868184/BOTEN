3030dc6d6d68e88a3ae5db631f92b6c8e2b34f3f
gn01868184 <gn01868184@gmail.com>
2021-03-04 12:00:49 +0800

    Create BotenSwagger.java

---
+++ src/main/java/ntou/cs/sose/entity/BotenSwagger.java
@@ -0,0 +1,16 @@
+package ntou.cs.sose.entity;
+
+import org.json.JSONObject;
+
+public class BotenSwagger {
+	private JSONObject swagger;
+
+	public JSONObject getSwagger() {
+		return swagger;
+	}
+
+	public void setSwagger(JSONObject swagger) {
+		this.swagger = swagger;
+	}
+
+}
