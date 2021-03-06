0e18366fd04f56bc15a18bbf346c15d6b87982fd
gn01868184 <gn01868184@gmail.com>
2021-03-06 12:57:35 +0800

    feat: JSONObject
    add chatbotEnabledSwaggerErrors, inputOutputConfig, chatbotConfigurator;

--- src/main/java/ntou/cs/sose/entity/BotenSwagger.java
+++ src/main/java/ntou/cs/sose/entity/BotenSwagger.java
@@ -4,6 +4,9 @@ import org.json.JSONObject;
 
 public class BotenSwagger {
 	private JSONObject swagger;
+	private JSONObject chatbotEnabledSwaggerErrors;
+	private JSONObject inputOutputConfig;
+	private JSONObject chatbotConfigurator;
 
 	public JSONObject getSwagger() {
 		return swagger;
@@ -13,4 +16,27 @@ public class BotenSwagger {
 		this.swagger = swagger;
 	}
 
+	public JSONObject getChatbotEnabledSwaggerErrors() {
+		return chatbotEnabledSwaggerErrors;
+	}
+
+	public void setChatbotEnabledSwaggerErrors(JSONObject chatbotEnabledSwaggerErrors) {
+		this.chatbotEnabledSwaggerErrors = chatbotEnabledSwaggerErrors;
+	}
+
+	public JSONObject getInputOutputConfig() {
+		return inputOutputConfig;
+	}
+
+	public void setInputOutputConfig(JSONObject inputOutputConfig) {
+		this.inputOutputConfig = inputOutputConfig;
+	}
+
+	public JSONObject getChatbotConfigurator() {
+		return chatbotConfigurator;
+	}
+
+	public void setChatbotConfigurator(JSONObject chatbotConfigurator) {
+		this.chatbotConfigurator = chatbotConfigurator;
+	}
 }
