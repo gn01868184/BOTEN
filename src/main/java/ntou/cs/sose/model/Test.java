9cc9fa8b3ee1bcdca58ed974dc0c4a1d458a11ce
gn01868184 <gn01868184@gmail.com>
2021-03-09 16:53:23 +0800

    test: RasaConfigurator

--- src/main/java/ntou/cs/sose/model/Test.java
+++ src/main/java/ntou/cs/sose/model/Test.java
@@ -3,19 +3,37 @@ package ntou.cs.sose.model;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import com.google.gson.Gson;
+
+import ntou.cs.sose.entity.BotenSwagger;
+
 public class Test {
 	public static void main(String[] args) {
+		Gson gson = new Gson();
+		
 		Test te = new Test();
-	    Class tes = te.getClass();
+		Class tes = te.getClass();
 		JSONObject swagger = new JSONObject(new JSONTokener(tes.getResourceAsStream("/foursquareSwagger.json")));
+
+		BotenSwagger bs = new BotenSwagger();
+		bs.setSwagger(swagger);
+
 		SwaggerChecker sc = new SwaggerChecker();
-		sc.swaggerChecker(swagger);
-		
 		InputOutputHandler ioc = new InputOutputHandler();
-		ioc.inputOutputHandler(swagger);
-		
 		ChatbotConfigurator cc = new ChatbotConfigurator();
-		cc.chatbotConfigurator(swagger);
-		System.out.println(cc.botenConfig());
+		RasaConfigurator rc = new RasaConfigurator();
+		
+		sc.swaggerChecker(bs);
+		
+		String inputOutputConfig = gson.toJson(ioc.inputOutputHandler(bs));
+		bs.setInputOutputConfig(new JSONObject(inputOutputConfig));
+		
+		cc.chatbotConfigurator(bs);
+		
+		bs.setNlu(rc.nluConfigurator(bs.getInputOutputConfig()));
+		bs.setStories(rc.storiesConfigurator(bs.getInputOutputConfig()));
+		
+		System.out.println(bs.getStories());
+
 	}
 }
