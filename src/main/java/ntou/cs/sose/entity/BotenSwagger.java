aeddc33f5fd4389873e09572d8975302c8e70fbf
gn01868184 <gn01868184@gmail.com>
2021-03-09 16:52:07 +0800

    feat: add nlu and stories class

--- src/main/java/ntou/cs/sose/entity/BotenSwagger.java
+++ src/main/java/ntou/cs/sose/entity/BotenSwagger.java
@@ -1,12 +1,21 @@
 package ntou.cs.sose.entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.jayway.jsonpath.JsonPath;
 
 public class BotenSwagger {
 	private JSONObject swagger;
 	private JSONObject chatbotEnabledSwaggerErrors;
 	private JSONObject inputOutputConfig;
 	private JSONObject chatbotConfigurator;
+	private String nlu;
+	private String stories;
 
 	public JSONObject getSwagger() {
 		return swagger;
@@ -38,5 +47,77 @@ public class BotenSwagger {
 
 	public void setChatbotConfigurator(JSONObject chatbotConfigurator) {
 		this.chatbotConfigurator = chatbotConfigurator;
+	}
+
+	public String getNlu() {
+		return nlu;
+	}
+
+	public void setNlu(String nlu) {
+		this.nlu = nlu;
+	}
+
+	public String getStories() {
+		return stories;
+	}
+
+	public void setStories(String stories) {
+		this.stories = stories;
+	}
+
+	public ArrayList allPath() {
+		org.json.JSONObject paths = swagger.getJSONObject("paths");
+		Iterator<String> keys = paths.keys();
+		ArrayList pathsArr = new ArrayList();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			pathsArr.add(key);
+		}
+		return pathsArr;
+	}
+
+	public ArrayList allFlow() {
+		ArrayList flowArr = new ArrayList();
+		try {
+			flowArr = JsonPath.read(swagger.toString(), "$.info..x-chatbotFlow..flowName");
+		} catch (ClassCastException e) {
+			System.out.println(e.getMessage());
+		}
+		return flowArr;
+	}
+
+	public HashMap<String, Object> chatbotFlow() {
+
+		JSONObject info = swagger.getJSONObject("info");
+		HashMap<String, Object> allFlow = new HashMap<String, Object>();
+		try {
+			org.json.JSONArray chatbotFlow = info.getJSONArray("x-chatbotFlow");
+			for (int i = 0; i < chatbotFlow.length(); i++) {
+				ArrayList pathsArr = new ArrayList();
+				org.json.JSONArray flowArray = chatbotFlow.getJSONObject(i).getJSONArray("flow");
+				String flowName = chatbotFlow.getJSONObject(i).getString("flowName");
+				for (int j = 0; j < flowArray.length(); j++) {
+					String flow = flowArray.getString(j);
+					pathsArr.add(flow);
+				}
+				allFlow.put(flowName, pathsArr);
+			}
+		} catch (JSONException e) {
+			System.out.println(e.getMessage());
+		}
+		return allFlow;
+	}
+
+	public static String changeSign(String pathName) {
+		// change / to _
+		ArrayList pathArr = new ArrayList();
+		String changedPath = "";
+		for (String retval : pathName.split("/")) {
+			if (!retval.equals("")) {
+				changedPath = changedPath + retval + "_";
+			}
+		}
+		changedPath = changedPath.substring(0, changedPath.length() - 1);
+		return changedPath;
 	}
 }
