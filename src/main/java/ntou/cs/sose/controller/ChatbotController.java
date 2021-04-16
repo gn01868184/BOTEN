888dadc9c42e0ed45bddf8f50312e2140f508f17
gn01868184 <gn01868184@gmail.com>
2021-04-16 12:16:37 +0800

    refactor: remove import

--- src/main/java/ntou/cs/sose/controller/ChatbotController.java
+++ src/main/java/ntou/cs/sose/controller/ChatbotController.java
@@ -1,8 +1,5 @@
 package ntou.cs.sose.controller;
 
-import java.util.Map;
-
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
